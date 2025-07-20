8a675192aab9de432b7fd658865e863a857cf228
Avinashgummalla-2005 <avinashgummalla2005@gmail.com>
2025-07-20 17:41:59 +0530

    App.js

--- Project files/frontend/App.js
+++ Project files/frontend/App.js
@@ -1,1 +1,17 @@
-// Frontend - React App.js placeholder
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Register from "./pages/Register";
+import BookAppointment from "./pages/BookAppointment";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/register" element={<Register />} />
+        <Route path="/book/:doctorId" element={<BookAppointment />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;// Frontend - React App.js placeholder
